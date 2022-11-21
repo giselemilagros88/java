926167d64ab5d61beed50b73ec37bf09cc3cef72
giselemilagros88 <92891602+giselemilagros88@users.noreply.github.com>
2022-11-21 05:03:12 -0300

    primer commit

---
+++ src/main/java/ar/com/codoacodo/Main.java
@@ -0,0 +1,12 @@
+package ar.com.codoacodo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// este es como el console.log pero de java 
+		System.out.println("Hola Mundo!!");
+
+	}
+
+}
